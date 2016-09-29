4de7aff6016374ef814f737e315737cb80c37a8d
Mek Buhl Nielsen <mek.nielsen@gmail.com>
2016-09-29 17:39:03 +0200

    Initial git add

---
+++ api/controllers/RsiteController.js
@@ -0,0 +1,11 @@
+/**
+ * RsiteController
+ *
+ * @description :: Server-side logic for managing rsites
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
